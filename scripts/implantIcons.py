e8ec54edcc887e129c7f35bd227ea399df33db42
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2024-05-20 23:33:02 -0700

    Refactor and insert all icons on plane_0
    Also rectify icon positioning, based on zoom level 2

--- scripts/implantIcons.py
+++ scripts/implantIcons.py
@@ -9,10 +9,6 @@ import time
 from collections import defaultdict
 from memory_profiler import memory_usage
 
-# For the highest zoom level we are dealing with images exceeding the openCV default limit
-# os.environ["OPENCV_IO_MAX_IMAGE_PIXELS"] = str(pow(2,40))
-# import cv2
-
 VERSION = "2024-04-10_a"
 DEFINITIONS_FILE_PATH = f"./osrs-wiki-maps/out/mapgen/versions/{VERSION}/minimapIcons.json"
 ICON_SPRITES_PATH = f"./osrs-wiki-maps/out/mapgen/versions/{VERSION}/icons"
@@ -42,18 +38,10 @@ def writeFile(image, zoomLevel, plane):
 	outputDir = os.path.normpath(os.path.join(OUTPUT_PATH, f"{zoomLevel}"))
 	if not os.path.exists(outputDir):
 		os.makedirs(outputDir)
-	# cv2.imwrite(os.path.join(outputDir, f"plane_{plane}.png"), image)
 	image.save(os.path.join(outputDir, f"plane_{plane}.png"))
 
 def implantIcons():
-	# Load the icon location definitions file 
-	# Load the icons
-	# Iterate through the definition file's data (z is plane)
-	# The most memory-intensive part will be pasting in the zoom 3 planes
-	# So maybe things should be separated by plane
-	# For each zoom level, paste the icons into the fullplane images using numpy 
-
-	# Process the icon data into per plane tuples
+	# Process the icon data into per plane data sets
 	iconLocations = defaultdict(list)
 	with open(DEFINITIONS_FILE_PATH) as iconJsonFile:
 		iconJson = json.load(iconJsonFile)
@@ -70,41 +58,52 @@ def implantIcons():
 	spriteImages = defaultdict(str)
 	for spriteFilePath in spriteFilePaths:
 		spriteID = int(os.path.splitext(os.path.basename(spriteFilePath))[0])
-		# spriteImage = cv2.imread(spriteFilePath, cv2.IMREAD_UNCHANGED)
 		spriteImage = Image.open(spriteFilePath)
 		spriteImages[spriteID] = spriteImage
-		# cv2.imshow("sprite", spriteImage)
-		# cv2.waitKey(0)
 	
 	# Paste sprites into their respective plane images
-	for plane in range(PLANE_COUNT):
-		iconCount = 0
-		for zoomLevel in range(ZOOM_LEVELS_WITH_ICONS[0], ZOOM_LEVELS_WITH_ICONS[1]+1):
-			# planeImage = cv2.imread(os.path.join(PLANE_FILE_PATH, f"{zoomLevel}/plane_{plane}.png"), cv2.IMREAD_UNCHANGED)
-			planeImage = Image.open(os.path.join(PLANE_FILE_PATH, f"{zoomLevel}/plane_{plane}.png"))
+	for plane in range(1, PLANE_COUNT):
+		implantIconsByZoom(plane, iconLocations, spriteImages)
 
-			# Scale factor used in coordinate transforms for this zoom level
-			scaleFactor = 2.0**zoomLevel / 2.0**BASELINE_ZOOM
+	# Plane 0 is intended to have ALL icons regardless of their level in the .json
+	implantIconsByZoom(0, iconLocations, spriteImages, ALL_ICONS=True)
+	
+def implantIconsByZoom(plane, iconLocations, spriteImages, ALL_ICONS=False):
+	for zoomLevel in range(ZOOM_LEVELS_WITH_ICONS[0], ZOOM_LEVELS_WITH_ICONS[1]+1):
+		planeImage = Image.open(os.path.join(PLANE_FILE_PATH, f"{zoomLevel}/plane_{plane}.png"))
 
-			# Iterate over all the icons in this plane
+		# Scale factor used in coordinate transforms for this zoom level
+		scaleFactor = 2.0**zoomLevel / 2.0**BASELINE_ZOOM
+
+		# If requesting all icons be drawn to this plane
+		if ALL_ICONS:
+			planeImage = drawAllIcons(planeImage, iconLocations, scaleFactor, spriteImages)
+		else:
+			# Otherwise only draw spec'd icons
 			for iconData in iconLocations[plane]:
-				iconXPos, iconYPos, iconSpriteID = iconData
-				# Coordinate transform to render origin
-				iconXPos = iconXPos - (LOWER_SQUARE_X * SQUARE_TILE_LENGTH)
-				iconYPos = iconYPos - (LOWER_SQUARE_Y * SQUARE_TILE_LENGTH)
+				planeImage = drawIcon(planeImage, iconData, scaleFactor, spriteImages)
+		writeFile(planeImage, zoomLevel, plane)
 
-				# Coordinate transform to pixels
-				iconXPosPX = int(iconXPos * TILE_PIXEL_LENGTH * scaleFactor - SPRITE_PIXEL_LENGTH/2 + 2)
-				iconYPosPX = int((planeHeight * scaleFactor) - (iconYPos * TILE_PIXEL_LENGTH * scaleFactor) - (TILE_PIXEL_LENGTH * scaleFactor) - SPRITE_PIXEL_LENGTH/2 + 2)
+def drawAllIcons(planeImage, iconLocations, scaleFactor, spriteImages):
+	# Draw all icons to this image regardless of z spec
+	for zSpec in range(PLANE_COUNT):
+		for iconData in iconLocations[zSpec]:
+			planeImage = drawIcon(planeImage, iconData, scaleFactor, spriteImages)
+	return planeImage
 
-				# Paste the image in
-				# planeImage[iconYPosPX:iconYPosPX+SPRITE_PIXEL_LENGTH, iconXPosPX:iconXPosPX+SPRITE_PIXEL_LENGTH] = spriteImages[iconSpriteID]
-				# planeImage.composite(spriteImages[iconSpriteID], mode=pv.enums.BlendMode.OVER, x=iconXPosPX, y=iconYPosPX)
-				# planeImage.paste(spriteImages[iconSpriteID], (iconXPosPX, iconYPosPX), spriteImages[iconSpriteID])
-				planeImage.alpha_composite(spriteImages[iconSpriteID], (iconXPosPX, iconYPosPX))
-				# cv2.rectangle(planeImage, (iconXPosPX, iconYPosPX), (iconXPosPX+SPRITE_PIXEL_LENGTH, iconYPosPX+SPRITE_PIXEL_LENGTH), (0,0,255), 1)
-			
-			writeFile(planeImage, zoomLevel, plane)
+def drawIcon(planeImage, iconData, scaleFactor, spriteImages):
+	iconXPos, iconYPos, iconSpriteID = iconData
+	# Coordinate transform to render origin
+	iconXPos = iconXPos - (LOWER_SQUARE_X * SQUARE_TILE_LENGTH)
+	iconYPos = iconYPos - (LOWER_SQUARE_Y * SQUARE_TILE_LENGTH)
+
+	# Coordinate transform to pixels
+	iconXPosPX = int(iconXPos * TILE_PIXEL_LENGTH * scaleFactor - SPRITE_PIXEL_LENGTH/2 - (1 * scaleFactor))
+	iconYPosPX = int((planeHeight * scaleFactor) - (iconYPos * TILE_PIXEL_LENGTH * scaleFactor) - (TILE_PIXEL_LENGTH * scaleFactor) - SPRITE_PIXEL_LENGTH/2 + (3 * scaleFactor)) #+3 because of the origin change and tile pixel length=4
+
+	# Paste the image in
+	planeImage.alpha_composite(spriteImages[iconSpriteID], (iconXPosPX, iconYPosPX))
+	return planeImage
 
 if __name__ == "__main__":
 	startTime = time.time()
