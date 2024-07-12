16e82d057b304d8d17525b6dc01fda699c67345b
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2024-07-12 08:18:38 -0700

    Centralize pyvips import logic

--- scripts/buildWikiMaps.py
+++ scripts/buildWikiMaps.py
@@ -6,16 +6,11 @@ import sys
 import json
 import glob
 
+# Pyvips import is OS-dependent, use dispatcher file
+from pyvips_import import pyvips as pv
+
 BASE_DIRECTORY = "osrs-wiki-maps/out/mapgen/versions"
 
-# runnerOS = system()
-# if runnerOS == "Windows":
-# Pyvips on windows is finnicky
-# Windows binaries are required: https://pypi.org/project/pyvips/, https://www.libvips.org/install.html
-LIBVIPS_VERSION = "8.15"
-vipsbin = os.path.join(os.getcwd(), f"vipsbin/vips-dev-{LIBVIPS_VERSION}/bin")
-os.environ['PATH'] = os.pathsep.join((vipsbin, os.environ['PATH']))
-import pyvips as pv
 
 def getCache(version=None):
 	# Fetch the latest, or the version supplied as an argument, game cache
